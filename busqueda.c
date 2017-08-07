99fcf23ebf4e5d5f4f7041718ee86106a71d4c24
Kevin Vásconez <kvasconezl@gmail.com>
2017-08-06 22:56:40 -0500

    Final

--- busqueda.c
+++ busqueda.c
@@ -4,12 +4,15 @@
 #include <string.h>
 #include <sys/time.h>
 #include <time.h>
+#include <unistd.h>
 
 #define MAX 100000
 
+extern char *strtok_r(char *str, const char *delim, char **saveptr);
+
 pthread_mutex_t mutex;
 char **palabras;
-int *num_palabras;
+int num_palabras[3] = {0, 0, 0};
 char *ruta;
 int *numCaracteresLinea;
 int bloque;
@@ -38,29 +41,38 @@ int numero_lineas(char *ruta, int *tam_lineas) {
     return -1;
 }
 
+void *mostrar_palabras(void *arg) {
+    while (1) {
+        sleep(1);
+        printf("--*-Palabras encontradas-*-- \nPalabra: %s - > Cantidad: %d\nPalabra: %s - > Cantidad: %d\nPalabra: %s - > Cantidad: %d\n",
+               *(palabras), num_palabras[0], *(palabras + 1), num_palabras[1], *(palabras + 2), num_palabras[2]);
+    }
+    return (void *) 0;
+}
+
 void *conteo(void *arg) {
     char *str = (char *) arg;
     char *token;
-    token = strtok(str, " \"!·$%&/()=º|@#~½¬<>-_'ç`+*[]{}ḉç¿,.!?:;");
+    token = strtok_r(str, " \"!·$%&/()=º|@#~½¬<>-_'ç`+*[]{}ḉç¿,.!?:;", &str);
 
     while (token != NULL) {
+        sleep(1);   //Comentar para ver resultados finales del conteo
         if (!strcmp(*(palabras), token)) {
             pthread_mutex_lock(&mutex);
-            *(num_palabras) = *(num_palabras) + 1;
+            num_palabras[0]++;
             pthread_mutex_unlock(&mutex);
         } else if (!strcmp(*(palabras + 1), token)) {
             pthread_mutex_lock(&mutex);
-            *(num_palabras + 1) = *(num_palabras + 1) + 1;
+            num_palabras[1]++;
             pthread_mutex_unlock(&mutex);
         } else if (!strcmp(*(palabras + 2), token)) {
             pthread_mutex_lock(&mutex);
-            *(num_palabras + 2) = *(num_palabras + 2) + 1;
+            num_palabras[2]++;
             pthread_mutex_unlock(&mutex);
         }
-        token = strtok(NULL, " \"!·$%&/()=º|@#~½¬<>-_'ç`+*[]{}ḉç¿,.!?:;");
+        token = strtok_r(NULL, " \"!·$%&/()=º|@#~½¬<>-_'ç`+*[]{}ḉç¿,.!?:;", &str);
     }
 
-    printf("--*-Palabras encontradas-*-- \nPalabra: %s - > Cantidad: %d\nPalabra: %s - > Cantidad: %d\nPalabra: %s - > Cantidad: %d\n", *(palabras), *(num_palabras), *(palabras + 1), *(num_palabras + 1), *(palabras + 2), *(num_palabras + 2));
     return (void *) 0;
 }
 
@@ -70,10 +82,8 @@ int main(int argc, char *argv[]) {
         exit(-1);
     }
 
-    //Aqui esta el arreglo de las palabras
     palabras = malloc(3 * sizeof(char *));
-    num_palabras = malloc(3 * sizeof(int));
-    //Le doy 0 al arreglo de enteros y le doy las palabras al arreglo de palabras
+
     for (int i = 0; i < 3; i++) {
         *(palabras + i) = argv[3 + i];
         *(num_palabras + i) = 0;
@@ -105,12 +115,20 @@ int main(int argc, char *argv[]) {
         seek = seek + bloque;
     }
 
+    pthread_t *t_mostrar = malloc(sizeof(pthread_t));
+    int stat_mostrar1, stat_mostrar2;
     int status;
     pthread_t *threads = malloc(hilos * sizeof(pthread_t));
 
+    stat_mostrar1 = pthread_create(t_mostrar, NULL, mostrar_palabras, NULL);
+
+    if (stat_mostrar1 != 0) {
+        fprintf(stderr, "Error al crear el hilo para mostrar palabras.\n");
+    }
+
     for (int i = 0; i < hilos; i++) {
         status = pthread_create(threads + i, NULL, conteo, (void *) *(parrafos + i));
-        if (status < 0) {
+        if (status != 0) {
             fprintf(stderr, "Error al crear el hilo : %d\n", i);
             exit(-1);
         }
@@ -118,10 +136,15 @@ int main(int argc, char *argv[]) {
 
     for (int j = 0; j < hilos; j++) {
         int status1 = pthread_join(threads[j], NULL);
-        if (status1 < 0) {
+        if (status1 != 0) {
             fprintf(stderr, "Error al esperar por el hilo\n");
             exit(-1);
         }
+    }
+
+    stat_mostrar2 = pthread_join(*t_mostrar, NULL);
+    if (stat_mostrar2 != 0) {
+        fprintf(stderr, "Error al esperar por el hilo para mostrar palabras.\n");
     }
 
     return (0);
